a902e88948257dbd387e259e41a588c39791dc99
Imaad Uddin <imaaduddin@icloud.com>
2021-07-11 22:50:29 -0500

    two sum solution

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,2 +1,12 @@
 # Question url: https://leetcode.com/problems/two-sum/
 
+# Question difficulty is easy
+
+# Solution from another use on LeetCode:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len (nums)):
+                if nums [i] + nums[j] == target:
+                    return[i, j]
+        return []
