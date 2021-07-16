d2a73997310b7d54abe2d59266658b7ff38d2969
Imaad Uddin <imaaduddin@icloud.com>
2021-07-16 00:07:22 -0500

    solution

--- Reverse-Integer.py
+++ Reverse-Integer.py
@@ -1,3 +1,15 @@
 # Question url: https://leetcode.com/problems/reverse-integer/
 
 # Question difficulty is easy.
+
+# Solution from a fellow LC member.
+class Solution:
+    def reverse(self, x: int) -> int:
+        output = int(str(abs(x))[::-1])
+        if output < 2**31:
+            if x >= 0:
+                return output
+            else:
+                return -output
+        else:
+            return 0
