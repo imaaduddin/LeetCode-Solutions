78c2439d37815887fd728ef49222447a907befd3
Imaad Uddin <imaaduddin@icloud.com>
2021-07-25 01:03:03 -0500

    solution

--- Reverse-Integer.py
+++ Reverse-Integer.py
@@ -31,3 +31,21 @@ class Solution:
         elif(x < 0):
             return ans * -1
         return ans
+
+
+# Another solution:
+class Solution:
+    def reverse(self, x):
+        sum = 0
+        sign = 1
+        if x < 0:
+            sign = -1
+            x = x * -1
+        while x > 0:
+            rem = x % 10
+            sum = sum * 10 + rem
+            x = x // 10
+        if not -2147483648 < sum < 2147483647:
+            return 0
+        return sign * sum
+        
