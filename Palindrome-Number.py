66f6faaf4a96367dc6e33151fcede67508d50ea4
Imaad Uddin <imaaduddin@icloud.com>
2021-07-29 00:42:32 -0500

    plindrome solution python

--- Palindrome-Number.py
+++ Palindrome-Number.py
@@ -1,3 +1,11 @@
 # Question url: https://leetcode.com/problems/palindrome-number/
-# Difficulty is easy.
+# Difficulty is easy, question number 9 on LeetCode.
 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False
+
+# Solution not converting integer to string
