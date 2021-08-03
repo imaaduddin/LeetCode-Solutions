cb7ec80af012ed917f04784454adf7d36f6859f4
Imaad Uddin <imaaduddin@icloud.com>
2021-08-02 23:52:47 -0500

    another solution for palindrome

--- Palindrome-Number.py
+++ Palindrome-Number.py
@@ -9,3 +9,15 @@ class Solution:
             return False
 
 # Solution not converting integer to string
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        reverse_num = 0
+        digit = 0
+        
+        while(x // (10 ** digit) != 0):
+            reverse_num = (reverse_num * 10) + (x // (10 ** digit) % 10)
+            digit += 1
+        return (x == reverse_num)
