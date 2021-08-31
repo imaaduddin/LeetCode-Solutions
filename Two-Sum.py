db63817426d819d90872af5fb98ac17e0c6e9c31
Imaad Uddin <imaaduddin@icloud.com>
2021-08-30 19:24:46 -0500

    saved file

--- Two-Sum.py
+++ Two-Sum.py
@@ -13,29 +13,29 @@ return []
 
 # Faster solution:
 # Solution source: https://youtu.be/2uWRxgN1Sbo
-complementMap = dict()
+# complementMap = dict()
             
-for i in range(len(nums)):
-    num = nums[i]
-    complement = target - num
-    if num in complementMap:
-        return[complementMap[num], i]
-    else:
-        complementMap[complement] = i
+# for i in range(len(nums)):
+#     num = nums[i]
+#     complement = target - num
+#     if num in complementMap:
+#         return[complementMap[num], i]
+#     else:
+#         complementMap[complement] = i
 
 
 # Another solution:
-for i in range(len(nums) - 1):
-    for j in range(i + 1, len(nums)):
-        if nums[i] + nums[j] == target:
-            return([i, j])
+# for i in range(len(nums) - 1):
+#     for j in range(i + 1, len(nums)):
+#         if nums[i] + nums[j] == target:
+#             return([i, j])
 
 
 # Another solution 
-seen = {}
+# seen = {}
 
-for i, num in enumerate(nums):
-    if target - num in seen:
-        return([seen[target - num], i])
-    elif num not in seen:
-        seen[num] = i
+# for i, num in enumerate(nums):
+#     if target - num in seen:
+#         return([seen[target - num], i])
+#     elif num not in seen:
+#         seen[num] = i
