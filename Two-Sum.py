b10f3369c25b14602a315c9fb6d9e41b56c2de1a
Imaad Uddin <imaaduddin@icloud.com>
2021-08-30 19:29:59 -0500

    two sum problem

--- Two-Sum.py
+++ Two-Sum.py
@@ -6,7 +6,7 @@
 # Brute force solution running O(n^2) 
 for i in range(len(nums)):
     for j in range(i + 1, len (nums)):
-        if nums [i] + nums[j] == target:
+        if nums[i] + nums[j] == target:
             return[i, j]
 return []
 
